9cc7f27970eff57c8e67b5388ef55715e093a736
linzheng <alidlg9045@126.com>
2022-11-18 15:36:11 +0800

    sidebar 样式修改

--- src/layouts/default/Header/index.vue
+++ src/layouts/default/Header/index.vue
@@ -3,11 +3,20 @@
 </script>
 
 <template>
-  <div>
-    header
+  <div class="header_content">
+    <div>
+      123
+    </div>
+    <div>
+      123
+    </div>
   </div>
 </template>
 
-<style scoped>
-
+<style lang="scss" scoped>
+.header_content{
+  width: 100%;
+  display: flex;
+  justify-content: space-between;
+}
 </style>
