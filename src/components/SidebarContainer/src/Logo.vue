6e70c335d06a9af9bd756579d33647c7c630cd51
linzheng <alidlg9045@126.com>
2022-11-21 14:55:12 +0800

    sidebar title 的 隐藏

--- src/components/SidebarContainer/src/Logo.vue
+++ src/components/SidebarContainer/src/Logo.vue
@@ -1,22 +1,34 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { storeToRefs } from 'pinia'
+import { useGlobalStatus } from '@/store/modules/globalStatus'
+
+const globalStatus = useGlobalStatus()
+const { isCollapse } = storeToRefs(globalStatus)
+</script>
 
 <template>
   <div class="sidebar_logo">
     <SvgIcon name="logo" size="24" />
-    <h2 class="sidebar_logo-title">
-      Admin-Template
-    </h2>
+    <span :class="{ 'hide-logo': isCollapse }">
+      <h2 class="sidebar_logo-title">
+        Admin-Template
+      </h2>
+    </span>
   </div>
 </template>
 
 <style lang="scss" scoped>
+@import '@/styles/variable.scss';
 .sidebar_logo {
   display: flex;
   align-items: center;
   justify-content: center;
-  height: 60px;
+  height: $sidebar-logo-height;
   &-title {
     margin-left: 5px;
   }
+}
+.hide-logo{
+  display: none;
 }
 </style>
